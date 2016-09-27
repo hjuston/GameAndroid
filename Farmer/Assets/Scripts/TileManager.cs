e6a438bb9c2d30df160d748069f9897c2d36645d
hjuston <kamil.ladyko@gmail.com>
2016-09-27 21:33:29 +0200

    0.0.4
    Poprawiono system wyszukiwania kratek. Nie działa najlepiej. Może go
    zmienić?

--- Farmer/Assets/Scripts/TileManager.cs
+++ Farmer/Assets/Scripts/TileManager.cs
@@ -7,49 +7,51 @@ public class TileManager : MonoBehaviour
 	public Tile CurrentTile;
 
     public GameObject cube;
-    public float SelectedCubeX = 0f;
-    public float SelectedCubeZ = 0f;
 
     public Material IdleTileMaterial;
     public Material SelectedTileMaterial;
-
-
+	
     public float CubeSize = 1;
-    public float buildingWidht = 3f;
-    public float buildingHeight = 2f;
-    
+    public float buildingWidht;
+    public float buildingHeight;
+
+	public Rotation rotation = Rotation.Horizontal;
+
+	public enum Rotation
+	{
+		Horizontal = 0,
+		Vertical = 1
+	}
+
+
     bool CheckCubes(GameObject focusedTile)
     {
-        Debug.Log("Pozycja przesłanego: X=" + focusedTile.transform.position.x + " Z=" + focusedTile.transform.position.z);
-        for (float x = focusedTile.transform.position.x ; x <= focusedTile.transform.position.x + buildingHeight; x++)
-        {
-            Debug.Log("PETLA 1: X=" + x);
-            for (float z = focusedTile.transform.position.z ; z >= focusedTile.transform.position.z - buildingWidht; z--)
-            {
-                Debug.Log("PETLA 2: Z=" + z);
-                Vector3 cubePosition = new Vector3(focusedTile.transform.position.x - x, focusedTile.transform.position.y, focusedTile.transform.position.z - z);
+		float focusedX = focusedTile.transform.position.x;
+		float focusedZ = focusedTile.transform.position.z;
 
-                // Puszczanie raya żeby sprawdzić czy na pozycji obok jest cube
-                Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.position - cubePosition);
+		// od miejsca kliknięcia do miejsca kliknięcia + szerokość
+		for (float x = focusedX; x <= focusedX + buildingWidht; x++)
+		{
+			// od miejsca kliknięcia do miejsca kliknięcia - wysokość (kratki zawsze ułożone są w dół
+			for(float z = focusedZ; z > focusedZ - buildingHeight; z--)
+			{
+				Vector3 cubePosition = new Vector3(x, 0f, z);
+				Vector3 direction = (cubePosition - Camera.main.transform.position).normalized;
 
-                Debug.Log(cubePosition);
-                Debug.Log("Casting ray");
-                Debug.DrawRay(ray.origin, ray.direction * 10000, Color.red, 20f);
+				Ray ray = new Ray(Camera.main.transform.position, direction);
+				RaycastHit hitInfo;
 
-                RaycastHit hitInfo;
-
-                if(!Physics.Raycast(ray, out hitInfo))
-                {
-                    return false;
-                }
-                else
-                {
-                    hitInfo.collider.gameObject.GetComponent<Renderer>().material = SelectedTileMaterial;
-                }
-            }
-        }
-
-        return true;
+				if (Physics.Raycast(ray, out hitInfo))
+				{
+					hitInfo.collider.gameObject.GetComponent<Renderer>().material = SelectedTileMaterial;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
+		return true;
     }
 
     void Update()
@@ -81,9 +83,6 @@ public class TileManager : MonoBehaviour
                 {
                     //Debug.DrawRay(ray.origin, ray.direction * 100, Color.yellow, 20f);
                     // Sprawdzanie czy kratki które są obok w podanych wymiarach są wolne
-
-                    SelectedCubeX = hitInfo.collider.gameObject.transform.localPosition.x;
-                    SelectedCubeZ = hitInfo.collider.gameObject.transform.localPosition.z;
 
                     if (CheckCubes(hitInfo.collider.gameObject))
                     {
