65c2a87eac881b33a6694a67d6e907bcdf09f613
hjuston <kamil.ladyko@gmail.com>
2016-09-28 20:24:11 +0200

    0.0.6
    Naprawiono system stawiania budowli na gridzie.

--- Farmer/Assets/GhostScript.cs
+++ Farmer/Assets/GhostScript.cs
@@ -1,12 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GhostScript : MonoBehaviour
 {
     public bool isGhost = false;
     public bool canPlace = true;
 
-    Collider[]
+	List<Collider> collisions = new List<Collider>();
 
     void FixedUpdate()
     {
@@ -26,17 +27,18 @@ public class GhostScript : MonoBehaviour
             Transform hitTransform = hitInfo.collider.gameObject.transform;
             float x = transform.localScale.x / 2;
             float z = transform.localScale.z / 2;
-            transform.position = new Vector3(hitTransform.position.x, 1f, hitTransform.position.z) + new Vector3(x, 0f, -z) - new Vector3(hitTransform.localScale.x /2 , 0f, -(hitTransform.localScale.z/2));
+            transform.position = new Vector3(hitTransform.position.x, 1f, hitTransform.position.z) + new Vector3(x, 0f, -z) - new Vector3(hitTransform.localScale.x /2 - 0.05f, 0f, -(hitTransform.localScale.z/2 - 0.05f));
         }
     }
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Collision");
         if (isGhost)
         {
-            if (other.gameObject.tag == "Player")
+            if ((other.gameObject.tag == "Player" || other.gameObject.tag == "nonselectable"))
             {
+				collisions.Add(other);
+
                 canPlace = false;
                 Helper.GetGameManager().CanPlace = false;
                 gameObject.GetComponent<Renderer>().material.SetColor("_Color", new Color(Color.red.r, Color.red.g, Color.red.b, 0.10f));
@@ -46,11 +48,16 @@ public class GhostScript : MonoBehaviour
 
     void OnTriggerExit(Collider other)
     {
-        if (isGhost && other.gameObject.tag == "Player")
+        if (isGhost && (other.gameObject.tag == "Player" || other.gameObject.tag == "nonselectable"))
         {
-            canPlace = true;
-            Helper.GetGameManager().CanPlace = true;
-            gameObject.GetComponent<Renderer>().material.SetColor("_Color", new Color(Color.green.r, Color.green.g, Color.green.b, 0.10f));
+			if (collisions.Contains(other)) collisions.Remove(other);
+
+			if (collisions.Count == 0)
+			{
+				canPlace = true;
+				Helper.GetGameManager().CanPlace = true;
+				gameObject.GetComponent<Renderer>().material.SetColor("_Color", new Color(Color.green.r, Color.green.g, Color.green.b, 0.10f));
+			}
         }
     }
 }
